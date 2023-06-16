85a85e52609ed18276d8c64c6717b6a0989b8f5b
jarangit <jaran.dch@gmail.com>
2023-06-17 04:02:09 +0700

    feat: add number function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp as Widget);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,20 +9,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "My app",
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Menu bar"),
-            ),
-            body: const Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("content1"),
-                Text("content1"),
-                Text("content1"),
-              ],
-            ))));
+    return const MaterialApp(
+      title: "My app",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "New app ",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Add Count"),
+          Text(
+            'Count: $count',
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              count += 1;
+            });
+          },
+          child: Icon(Icons.add)),
+    );
   }
 }
