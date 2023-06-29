eda00c51eca44e4fd01c2216994719c3abd25cb8
jarangit <jaran.dch@gmail.com>
2023-06-29 19:39:43 +0700

    feat: create fucntion validation form

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:my_app/models/profile.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,6 +10,9 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final formKey = GlobalKey<FormState>();
+  Profile profile = Profile(email: '', password: '');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +23,44 @@ class _RegisterScreenState extends State<RegisterScreen> {
           padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
           child: Container(
             child: Form(
+                key: formKey,
                 child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Email"),
-                TextFormField(),
-                Text("Password"),
-                TextFormField(),
-                SizedBox(
-                  width: double.infinity,
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Submit")),
-                )
-              ],
-            )),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Email"),
+                    TextFormField(
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: "Email is required"),
+                        EmailValidator(
+                            errorText: "Email format is not correct"),
+                      ]),
+                      keyboardType: TextInputType.emailAddress,
+                      onSaved: (email) {
+                        profile.email = email!;
+                      },
+                    ),
+                    Text("Password"),
+                    TextFormField(
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: "Email is required"),
+                      ]),
+                      obscureText: true,
+                      onSaved: (password) {
+                        profile.password = password!;
+                      },
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              formKey.currentState?.save();
+                            }
+                          },
+                          child: Text("Submit")),
+                    )
+                  ],
+                )),
           ),
         ));
   }
